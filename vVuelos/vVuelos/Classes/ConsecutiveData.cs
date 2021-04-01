390fc1681027cef4a6c1899600197a5129a1d694
Roger Jose Ulate Rivera <roger.p.designe357@gmail.com>
2021-04-01 09:54:15 -0600

    Cambios sencillos

--- vVuelos/vVuelos/Classes/ConsecutiveData.cs
+++ vVuelos/vVuelos/Classes/ConsecutiveData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using vVuelos.Models;
 
 namespace vVuelos.Classes
 {
@@ -14,6 +15,25 @@ namespace vVuelos.Classes
         public string prefix { get; set; }
         public int? range_int { get; set; }
         public int? range_out { get; set; }
+
+        public static List<ConsecutiveData> GetConsecutives()
+        {
+            using (vVuelosEntities dbLink = new vVuelosEntities())
+            {
+                return (from a in dbLink.consecutives
+                        select new ConsecutiveData
+                        {
+                            id = a.id,
+                            description = a.description,
+                            value = a.value,
+                            prefix = a.prefix,
+                            range_int = a.range_int,
+                            range_out = a.range_out
+                        }
+                        ).ToList();
+            }
+        }
+
 
     }
 }
--- vVuelos/vVuelos/Controllers/ConsecutivesController.cs
+++ vVuelos/vVuelos/Controllers/ConsecutivesController.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using vVuelos.Classes;
 
 namespace vVuelos.Controllers
 {
     public class ConsecutivesController : ApiController
     {
+
+        // DESDE UN API NO SE ACCEDE a la DATA
+
         // GET: api/Consecutives
-        public IEnumerable<string> Get()
+        public List<ConsecutiveData> Get()
         {
-            return new string[] { "value1", "value2" };
+            return ConsecutiveData.GetConsecutives();
         }
 
         // GET: api/Consecutives/5
